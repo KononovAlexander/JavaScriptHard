0d9fb6f01b243acc9a02abefd4d8fbfaa7dda75e
KononovAlexander <kononov8214@gmail.com>
2021-02-02 17:17:59 +0200

    lang switch added and name analisys added

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
-let num =  266219 ;
+'use strict';
+let lang = prompt('RU / EN ? if...else', 'ru');
 
-let numStr = String(num).split('');
-let sum = 1;
-
-for( let i = 1; i <= numStr.length; i++){
-
-    sum *= numStr[i - 1];
-    
+if(lang === 'en'){
+    console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+}else if(lang === 'ru'){
+    console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресение');
 }
-console.log('Сумма: ' + sum);
-console.log('Первые два числа: ' + String(sum**3).substr(0, 2));
 
 
+
+let lang2 = prompt('RU / EN ? switch', 'ru');
+
+switch(lang2){
+    case 'ru':
+        console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресение');
+        break;
+        case 'en':
+            console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+            break;
+        }
+        
+
+let lang3 = prompt('RU / EN ? array', 'ru');
+
+let langArr = ['ru',['Понедельник ', 'Вторник ', 'Среда ', 'Четверг ', 'Пятница ', 'Суббота ', 'Воскресение '],
+'en',['Monday ', 'Tuesday ', 'Wednesday ', 'Thursday ', 'Friday ', 'Saturday ', 'Sunday ']];
+let num = langArr.indexOf(lang3);
+
+console.log(langArr[num + 1].toString());
+
+
+let namePerson = prompt('Введите имя', 'Nemo');
+
+let result = namePerson === 'Артём' ? console.log('Директор') : 
+namePerson === 'Максим' ? console.log('Преподаватель') : console.log('Студент');;
